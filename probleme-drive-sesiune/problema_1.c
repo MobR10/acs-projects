989398a921d026e40c3e21db261e2b04e7a31300
MobR10 <anghelraffaele10@gmail.com>
2024-12-10 08:29:41 +0200

    update 8:29 10.12.2024

--- probleme-drive-sesiune/problema_1.c
+++ probleme-drive-sesiune/problema_1.c
@@ -10,18 +10,18 @@ typedef struct{
 
 }Comp;
 
-int functie(int v[], int n,int w[], int m,int rez[], int *p)
+int functie(int v[], size_t n,int w[], size_t m,int rez[], size_t *p)
 {
     *p=0; int ok=0;
-    for(int i=0;i<n;i++)
+    for(size_t i=0;i<n;i++)
         if(v[i]>0)
             {
                 ok=1;
                 rez[*p]=i;
                 (*p)++;
             }
-    for(int i=0;i<m;i++)
-        if(v[i]==0)
+    for(size_t i=0;i<m;i++)
+        if(w[i]==0)
             {
                 ok=1;
                 rez[*p]=i;
@@ -31,17 +31,33 @@ int functie(int v[], int n,int w[], int m,int rez[], int *p)
     return 1;
     else return 0;
 }
+
+void init()
+{
+    FILE *f;
+    if((f=fopen("fisier.dat","wb"))==NULL)
+        {
+            puts("ERROR init(): nu s-a putut deschide fisierul!");
+            exit(1);
+        }
+    int a[7]={1,2,3,4,5,6,7};   
+    fwrite(a,sizeof(int),7,f);
+    fclose(f);
+
+}
 int main()
 {
-    Comp t[20];
-    FILE *fb,*rezultat;
-    int n,m,p,v[20],w[20],rez[40];
+    //Comp t[20];
+    FILE *fb;
+    //FILE *rezultat;
+    int v[20],w[20],rez[40];
+    size_t n,m,p;
     
-
+    init();
     printf("Introduceti numarul de elemente al primului vector: ");
-    scanf("%d",&n);
+    scanf("%lld",&n);
     printf("Introduceti numarul de elemente al celui de-al doilea vector: ");
-    scanf("%d",&m);
+    scanf("%lld",&m);
 
     if((fb=fopen("fisier.dat","rb"))==NULL)
         {
@@ -51,10 +67,10 @@ int main()
     fread(v,sizeof(int),n,fb);
     fread(w,sizeof(int),m,fb);
 
-    for(int i=0;i<n;i++)
+    for(size_t i=0;i<n;i++)
         printf("%d ",v[i]);
     printf("\n");
-    for(int i=0;i<m;i++)
+    for(size_t i=0;i<m;i++)
         printf("%d ",w[i]);
 
     if(functie(v,n,w,m,rez,&p))
@@ -63,11 +79,13 @@ int main()
             printf("Introduceti cu cate fisiere doriti sa lucrati: ");
             scanf("%u",&nr);
             
-            for(int i=0;i<nr;i++)
+            for(unsigned i=0;i<nr;i++)
             {
                 printf("Introduceti numele fisierului %d:",i+1);
                 char nume[20];
                 scanf("%s",nume);
+                if(strcmp(nume,"quit")==0)
+                    exit(0);
                 FILE *f;
                 if((f=fopen(nume,"r"))==NULL)
                     {
@@ -77,7 +95,7 @@ int main()
                 else
                 {
                     char aux[40]="";
-                    for(int i=0;i<p;i++)
+                    for(size_t j=0;j<p;j++)
                         {
                             
                         }
