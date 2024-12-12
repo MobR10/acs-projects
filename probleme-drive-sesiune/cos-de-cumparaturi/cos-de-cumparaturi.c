d360a54dcbab401b52c9185bc50b4b3b67a160ff
MobR10 <100953132+MobR10@users.noreply.github.com>
2024-12-13 00:10:55 +0200

    update 00:10  13.12.2024

--- probleme-drive-sesiune/cos-de-cumparaturi/cos-de-cumparaturi.c
+++ probleme-drive-sesiune/cos-de-cumparaturi/cos-de-cumparaturi.c
@@ -10,136 +10,152 @@
 #define FISIER_INTRARE "intrare.txt"
 #define FISIER_PRODUSE "produse.dat"
 
-typedef struct{
-        char producator[50];
-        int stoc;
-        float pret;
-    }Informatii;
-
-typedef struct{
-    char numeProdus[50];
-    int cantitate;
-    Informatii varianta[20];
-}Produs;
-
-// SIMULARE OBIECTE DIN PROGRAMAREA ORIENTATA PE OBIECT
-void produs(Produs *produs, char numeProdus[],int cantitate,Informatii varianta[])
+typedef struct
 {
-        strcpy(produs->numeProdus,numeProdus);
-        produs->cantitate=cantitate;
-        for(int i=0;i<cantitate;i++)
-            {
-                strcpy(produs->varianta[i].producator,varianta[i].producator);
-                produs->varianta[i].stoc=varianta[i].stoc;
-                produs->varianta[i].pret=varianta[i].pret;
-            }
+    char producator[50];
+    int stoc;
+    float pret;
+} Produs;
+
+typedef struct
+{
+    char numeProdus[50];
+    int numarProduse;
+    Produs produs[20];
+} TipProdus;
+
+// SIMULARE OBIECTE DIN POO
+void constructTipProdus(TipProdus *tipProdus, char numeProdus[], int numarProduse, Produs produs[])
+{
+    strcpy(tipProdus->numeProdus, numeProdus);
+    tipProdus->numarProduse = numarProduse;
+    for (int i = 0; i < numarProduse; i++)
+    {
+        strcpy(tipProdus->produs[i].producator, produs[i].producator);
+        tipProdus->produs[i].stoc = produs[i].stoc;
+        tipProdus->produs[i].pret = produs[i].pret;
     }
+}
 
 // CREAM SI NOI NISTE PRODUSE IN MAGAZIN CA SA AVEM CE SA VERIFICAM
-void init(Produs produse[],int cantitate)
+void init(TipProdus tipProdus[], int numarTipProduse)
 {
     FILE *f;
-    if((f=fopen(FISIER_PRODUSE,"wb"))==NULL)
-        {
-            puts("Nu s-a putut deschide fisierul"
-            FISIER_PRODUSE);
-            exit(1);
-        }
+    if ((f = fopen(FISIER_PRODUSE, "wb")) == NULL)
+    {
+        puts("Eroare deschidere fisier " FISIER_PRODUSE " in functia init() ");
+        exit(1);
+    }
     // SCRIEM IN FISIER PRODUSELE
-    fwrite(produse,sizeof(Produs),(size_t)cantitate,f);
+    fwrite(tipProdus, sizeof(TipProdus), (size_t)numarTipProduse, f);
     fclose(f);
 }
 
-void citire(Produs produse[],int *n)
+void citire(TipProdus tipProdus[], int *numarTipProduse)
 {
-    printf("Cate produse? ");
-    do{
-      scanf("%d",n);
-      if((*n)<=0||(*n)>5)
-        puts("Introdu un numar natural nenul cel mult 5: ");  
-    }while((*n)<=0||(*n)>5);
-    
+    printf("Cate tipuri de produse? ");
+    do
+    {
+        scanf("%d", numarTipProduse);
+        if ((*numarTipProduse) <= 0 || (*numarTipProduse) > 5)
+            puts("Introdu un numar natural nenul cel mult 5: ");
+    } while ((*numarTipProduse) <= 0 || (*numarTipProduse) > 5);
+
     getchar();
-    for(int i=0;i<(*n);i++)
+    for (int i = 0; i < (*numarTipProduse); i++)
+    {
+        char numeProdus[50];
+        int numarProduse;
+        printf("Introdu numele tipului de produs: ");
+        gets(numeProdus);
+        printf("Introdu cate variante ale produsului sa fie:");
+        do
         {
-            char numeProdus[50];
-            int variante;
-            printf("Introdu numele produsului: ");
-            gets(numeProdus);
-            printf("Introdu cate variante ale produsului sa fie:");
-            do{
-                scanf("%d",&variante);
-                if(variante<=0||variante>3)
-                    puts("Te rog sa introduci un numar natural nenul cel mult 3: ");
-            }while(variante<=0||variante>3);
+            scanf("%d", &numarProduse);
+            if (numarProduse <= 0 || numarProduse > 3)
+                puts("Te rog sa introduci un numar natural nenul cel mult 3: ");
+        } while (numarProduse <= 0 || numarProduse > 3);
+        getchar();
+        Produs produs[3];
+        printf("Acum introdu %d variante diferite de %s:\n", numarProduse, numeProdus);
+        for (int j = 0; j < numarProduse; j++)
+        {
+            char producator[50];
+            int stoc;
+            float pret;
+            printf("Varianta %d:\nProducator: ", j + 1);
+            gets(producator);
+            strcpy(produs[j].producator, producator);
+            printf("Stoc: ");
+            scanf("%d", &stoc);
+            produs[j].stoc = stoc;
+            printf("Pret: ");
+            scanf("%f", &pret);
+            produs[j].pret = pret;
             getchar();
-            Informatii info[3];
-            printf("Acum introdu %d feluri diferite de %s:\n",variante,numeProdus);
-            for(int j=0;j<variante;j++)
-            {
-                char producator[50];
-                int stoc;
-                float pret;
-                printf("Varianta %d:\nProducator: ",j+1);
-                gets(producator);
-                strcpy(info[j].producator,producator);
-                printf("Stoc: ");
-                scanf("%d",&stoc);
-                info[j].stoc=stoc;
-                printf("Pret: ");
-                scanf("%f",&pret);
-                info[j].pret=pret;
-                getchar();
-            }
-            produs(&produse[i],numeProdus,variante,info);            
         }
-    init(produse,*n);
+        constructTipProdus(&tipProdus[i], numeProdus, numarProduse, produs);
+    }
+    init(tipProdus, *numarTipProduse);
 }
 
-void sortProdusCantitate(Produs *produs)
+void sortProduseDupaCantitate(TipProdus *tipProdus)
 {
-    Informatii *lista=produs->varianta;
-    for(int i=0;i<produs->cantitate-1;i++)
+    Produs *produs = tipProdus->produs;
+    for (int i = 0; i < tipProdus->numarProduse - 1; i++)
+    {
+        for (int j = i + 1; j < tipProdus->numarProduse; j++)
         {
-            for(int j=i+1;j<produs->cantitate;j++)
-                {
-                    if(lista[i].stoc > lista[j].stoc)
-                    {
-                        Informatii aux=lista[i];
-                        lista[i]=lista[j];
-                        lista[j]=aux;   
-                    }
-                }
+            if (produs[i].stoc > produs[j].stoc)
+            {
+                Produs aux = produs[i];
+                produs[i] = produs[j];
+                produs[j] = aux;
+            }
         }
+    }
 }
 
 int main()
-{
-    Produs produse[5];
-    int nr_produse;
 
-    citire(produse,&nr_produse);
-    sortProdusCantitate(&produse[0]);
+{
+    int numarTipProduse;
+    TipProdus tipProdus[5];
+    /*
+    PRODUSE.DAT ESTE FISIERUL CU INFORMATIILE DESPRE PRODUSELE DIN MAGAZIN. DEJA ARE PRODUSELE INAUNTRU, NU E NEVOIE SA CITITI DE LA 0, DECAT DACA VREI! RECOMAND COPIEREA FISIERULUI INAINTE DE ASTA!
+    AICI E ZONA DE INITIALIZARE A PRODUSELOR DIN MAGAZIN. LA EXAMEN NU VEI AVEA NICIUN FISIER CU DATE DIN CARE SA PREIEI INFORMATIA CARE TI SE CERE IN ENUNT, CI TU TREBUIE SA REZOLVI CA SI CAND ELE AR EXISTA, DAR FARA SA COMPILEZI, EVIDENT, PENTRU CA ITI VA DA ORICUM EROARE
+    DACA NU II DAI NISTE DATE SA PRELUCREZE. IN CEL MAI FERICIT CAZ, DACA TERMINI ALGORITMUL SI MAI AI TIMP, POTI DA FACE SCURT NISTE DATE DE INTRARE CA SA PROBEZI CORECTITUDINEA REZULTATULUI, DAR NU PIERDE TIMPUL PE ASA CEVA, CA NICI MACAR NU SE PUNCTEAZA COMPILAREA!!!
+    IN EXAMEN NU TREBUIE COMPILAT PROGRAMUL, CI CONTEAZA SA SCRII CAT MAI CORECT SI LOGIC ALGORITMUL FARA A MAI COMPILA, DAR ACUM POTI SA SI TESTEZI CUM MERGE PROGRAMUL!
+    */
+
+    // UNCOMMENT PENTRU A SCHIMBA LISTA DE PRODUSE DIN MAGAZIN (CITESTE FIECARE PRODUS DE LA TASTATURA
+    // citire(tipProdus,&numarTipProduse);
+
+    // CITIRE PRODUSE DIN PRODUSE.DAT SI AMPLASAREA LOR IN VECTORUL CU PRODUSE
     FILE *f;
-    if((f=fopen(FISIER_PRODUSE,"rb"))==NULL)
-        {
-            puts("Eroare deschidere citire fisier cu produse!");
-            exit(1);
-        }
-    printf("\n==============AFISARE PRODUSE==============\n");
-    for(int i=0;i<nr_produse;i++)
+    if ((f = fopen(FISIER_PRODUSE, "rb")) == NULL)
     {
-        Produs p;
-        fread(&p,sizeof(Produs),1,f);
-        printf("Produsul numarul %d:{\n",i+1);
-        printf("Nume produs: %s\nTipuri de %s: %d\n",p.numeProdus,p.numeProdus,p.cantitate);
-        for(int j=0;j<p.cantitate;j++)
-            {
-                printf("Tipul %d:{\nProducator: %s\nStoc: %d\nPret: %.2f\n}",j+1,p.varianta[j].producator,p.varianta[j].stoc,p.varianta[j].pret);
-            }
-        printf("\n");
+        puts("Eroare deschidere citire fisier cu produse!");
+        exit(1);
     }
+    fseek(f, 0, SEEK_END);
+    numarTipProduse = ftell(f) / sizeof(TipProdus);
+    fseek(f, 0, SEEK_SET);
+    fread(tipProdus, sizeof(TipProdus), numarTipProduse, f);
     fclose(f);
+
+    // FOR TESTING PURPOSES ONLY, DACA NU AFISEAZA CORECT DUPA O NOUA CITIRE, IT'S YOUR FAULT!
+     printf("\n==============AFISARE PRODUSE==============\n");
+     for(int i=0;i<numarTipProduse;i++)
+     {
+         printf("Produsul numarul %d:{\n",i+1);
+         printf("Nume produs: %s\nTipuri de %s: %d\n",tipProdus[i].numeProdus,tipProdus[i].numeProdus,tipProdus[i].numarProduse);
+         for(int j=0;j<tipProdus[i].numarProduse;j++)
+             {
+                 printf("Tipul %d:{\nProducator: %s\nStoc: %d\nPret: %.2f\n}",j+1,tipProdus[i].produs[j].producator,tipProdus[i].produs[j].stoc,tipProdus[i].produs[j].pret);
+             }
+         printf("\n");
+     }
 
     return 0;
 }
