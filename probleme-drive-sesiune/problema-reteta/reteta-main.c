3dafb3ed943c96a2d3207bf044c2f3982f2e6ddc
MobR10 <100953132+MobR10@users.noreply.github.com>
2025-01-07 19:33:32 +0200

    problema cu retete done (maybe 99,99%)

--- probleme-drive-sesiune/problema-reteta/reteta-main.c
+++ probleme-drive-sesiune/problema-reteta/reteta-main.c
@@ -2,13 +2,102 @@
 #include <stdlib.h>
 #include <string.h>
 
-#define DEBUG 1
+// CHESTII DE DEBUG
+#define DEBUG 0
+#define CREEAZA_CAMARA 0
+//===============
 
+// ASTA NU E DEBUG
 typedef struct
 {
     char denumire[50];
     float cantitate;
 } Ingredient;
+//================
+
+// MORE DEBUG
+#if DEBUG == 1
+void afiseazaLista(Ingredient v[], int lungime)
+{
+    for (int i = 0; i < lungime; i++)
+    {
+        printf("Ingredientul %d:\nDenumire: %s\n", i + 1, v[i].denumire);
+        printf("Cantitate: %.2f\n", v[i].cantitate);
+    }
+}
+
+void afiseazaCamara()
+{
+    FILE *camara;
+    if ((camara = fopen("camara.dat", "rb")) == NULL)
+    {
+        puts("Eroare la creearea fisierului camara.dat in functia creeazaCamara");
+        exit(1);
+    }
+    puts("\nafiseazaCamara()\n");
+    Ingredient lista[15];
+    int lungime;
+    fseek(camara, 0L, SEEK_END);
+    lungime = (int)(ftell(camara) / (long)sizeof(Ingredient));
+    fseek(camara, 0L, SEEK_SET);
+    fread(lista, sizeof(Ingredient), (size_t)lungime, camara);
+    afiseazaLista(lista, lungime);
+    fclose(camara);
+    puts("\nSFARSIT\n");
+}
+#endif
+
+#if CREEAZA_CAMARA == 1
+void creeazaCamara()
+{
+    FILE *camara;
+    if ((camara = fopen("camara.dat", "wb")) == NULL)
+    {
+        puts("Eroare la creearea fisierului camara.dat in functia creeazaCamara");
+        exit(1);
+    }
+
+    puts("\ncreeazaCamara()\n");
+    Ingredient lista[15];
+    char denumire[50], aux[50], c;
+    float cantitate;
+    int j = 1, lungime, pos;
+    while (j)
+    {
+        printf("Ingredientul %d: ", j);
+        if ((c = getchar()) == '-')
+        {
+            getchar();
+            j = 0;
+        }
+        else
+        {
+            scanf("%s", denumire);
+            scanf("%f", &cantitate);
+            getchar();
+            strcpy(aux, denumire);
+            strcpy(denumire + 1, aux);
+            denumire[0] = c;
+
+            if ((pos = esteInLista(lista, lungime, denumire)) != -1)
+            {
+                lista[pos].cantitate += cantitate;
+            }
+            else
+            {
+                strcpy(lista[lungime].denumire, denumire);
+                lista[lungime].cantitate = cantitate;
+                lungime++;
+            }
+            j++;
+        }
+    }
+    fwrite(lista, sizeof(Ingredient), lungime, camara);
+    fclose(camara);
+    puts("\nSFARSIT APEL\n");
+}
+#endif
+//========================
 
 int esteInLista(Ingredient lista[15], int lungime, char *denumire)
 {
@@ -17,7 +106,8 @@ int esteInLista(Ingredient lista[15], int lungime, char *denumire)
             return i;
     return -1;
 }
-void citesteRetete(Ingredient lista[], int *lungime)
+
+void citesteReteteTastatura(Ingredient lista[], int *lungime)
 {
     // CITIRE DE LA TASTATURA
     int numarRetete;
@@ -30,11 +120,11 @@ void citesteRetete(Ingredient lista[], int *lungime)
         if (numarRetete <= 0 || numarRetete >= 5)
             printf("Introduceti un numar natural nenul mai mic decat 5!");
     } while (numarRetete <= 0 || numarRetete >= 5);
-    printf("Acum introduceti ingredientele fiecarei retete dupa formatul <denumire> <cantitate>.\n"
+    printf("\nAcum introduceti ingredientele fiecarei retete dupa formatul <denumire> <cantitate>.\n"
            "Daca doriti sa introduceti mai multe ingrediente, apasati doar <ENTER> la final, fara spatii.\n"
            "Daca doriti sa introduceti mai multe retete, le despartiti printr-un '-'\n");
     int i, j, pos;
-    char denumire[50], temp[50];
+    char denumire[50], aux[50], c;
     float cantitate;
     for (i = 0; i < numarRetete; i++)
     {
@@ -43,23 +133,33 @@ void citesteRetete(Ingredient lista[], int *lungime)
         while (j)
         {
             printf("Ingredientul %d: ", j);
-            scanf("%s", denumire);
-            scanf("%f", &cantitate);
-            if ((pos = esteInLista(lista, *lungime, denumire)) != -1)
-                lista[pos].cantitate += cantitate;
+            if ((c = getchar()) == '-')
+            {
+                getchar();
+                j = 0;
+            }
             else
             {
-                strcpy(lista[*lungime].denumire, denumire);
-                lista[*lungime].cantitate = cantitate;
-                (*lungime)++;
-            }
-            int ok = 1;
-            do
-            {
-                gets(temp);
+                scanf("%s", denumire);
+                scanf("%f", &cantitate);
                 getchar();
-                puts(temp);
-            } while (strlen(temp) != 1);
+                strcpy(aux, denumire);
+                strcpy(denumire + 1, aux);
+                denumire[0] = c;
+
+                if ((pos = esteInLista(lista, *lungime, denumire)) != -1)
+                {
+                    lista[pos].cantitate += cantitate;
+                }
+                else
+                {
+                    strcpy(lista[*lungime].denumire, denumire);
+                    lista[*lungime].cantitate = cantitate;
+                    (*lungime)++;
+                }
+
+                j++;
+            }
         }
     }
 }
@@ -73,64 +173,76 @@ int existaAvertizari(int lungime)
 
 int functie(Ingredient listaCamara[15], int lungimeCamara, Ingredient listaNecesare[15], int lungimeNecesare, char listaAvertizare[][50], int *lungimeAvertizare, Ingredient listaCumparaturi[15], int *lungimeCumparaturi)
 {
-    FILE *camara;
-    if ((camara = fopen("camara.dat", "wb")) == NULL)
-    {
-        puts("Eroare la deschiderea fisierului camara.dat in 'functie'!");
-        exit(1);
-    }
     *lungimeAvertizare = 0;
     *lungimeCumparaturi = 0;
     int pos, toateIngredientele = 1;
     for (int i = 0; i < lungimeNecesare; i++)
     {
         pos = esteInLista(listaCamara, lungimeCamara, listaNecesare[i].denumire);
-        if (pos != 1 && listaCamara[pos].cantitate > listaNecesare[i].cantitate)
+        if (pos != -1 && listaCamara[pos].cantitate > listaNecesare[i].cantitate)
         {
+            FILE *camara;
+            if ((camara = fopen("camara.dat", "wb")) == NULL)
+            {
+                puts("Eroare la deschiderea fisierului camara.dat in 'functie'! (primul if)");
+                exit(1);
+            }
             listaCamara[pos].cantitate -= listaNecesare[i].cantitate;
-            fwrite(listaCamara, sizeof(Ingredient), (long)lungimeCamara, camara);
-            fseek(camara, 0L, SEEK_SET);
+            fwrite(listaCamara, sizeof(Ingredient), (size_t)lungimeCamara, camara);
+            fclose(camara);
         }
-        else if (pos != 1 && listaCamara[pos].cantitate == listaNecesare[i].cantitate)
+        else if (pos != -1 && listaCamara[pos].cantitate == listaNecesare[i].cantitate)
         {
+            FILE *camara;
+            if ((camara = fopen("camara.dat", "wb")) == NULL)
+            {
+                puts("Eroare la deschiderea fisierului camara.dat in 'functie' (al doilea else if)!");
+                exit(1);
+            }
+            strcpy(listaAvertizare[*lungimeAvertizare], listaCamara[pos].denumire);
+            (*lungimeAvertizare)++;
             for (int j = pos; j < lungimeCamara - 1; j++)
                 listaCamara[j] = listaCamara[j + 1];
             lungimeCamara--;
-            fwrite(listaCamara, sizeof(Ingredient), (long)lungimeCamara, camara);
-            fseek(camara, 0L, SEEK_SET);
-            strcpy(listaAvertizare[*lungimeAvertizare], listaCamara[pos].denumire);
-            (*lungimeAvertizare)++;
+            fwrite(listaCamara, sizeof(Ingredient), (size_t)lungimeCamara, camara);
+            fclose(camara);
         }
         else
         {
             listaCumparaturi[*lungimeCumparaturi] = listaNecesare[i];
+            if (pos != 1)
+                listaCumparaturi[*lungimeCumparaturi].cantitate = listaNecesare[i].cantitate - listaCamara[pos].cantitate;
+            else
+                listaCumparaturi[*lungimeCumparaturi].cantitate = listaNecesare[i].cantitate;
             (*lungimeCumparaturi)++;
             toateIngredientele = 0;
         }
     }
-    fclose(camara);
     return toateIngredientele;
 }
 
-#if DEBUG == 1
-void afiseazaLista(Ingredient v[], int lungime)
-{
-    for (int i = 0; i < lungime; i++)
-    {
-        printf("Denumire: %s\n", v[i].denumire);
-        printf("Cantitate: %f\n", v[i].cantitate);
-    }
-}
-#endif
-
 int main()
 {
-    // AFLAM CATE INGREDIENTE EXISTA SI LE PUNEM INTR-UN ARRAY
-    Ingredient listaCamara[15], listaNecesare[15], listaCumparaturi[15];
-    char listaAvertizare[15][50];
-    int lungimeCamara, lungimeNecesare = 0, lungimeAvertizare, lungimeCumparaturi;
+#if CREEAZA_CAMARA == 1
+    creeazaCamara();
+#endif
 
-    citesteRetete(listaNecesare, &lungimeNecesare);
+#if DEBUG == 1
+    afiseazaCamara();
+#endif
+
+    Ingredient listaCamara[15], listaNecesare[15], listaCumparaturi[15], recuperare[15];
+    char listaAvertizare[15][50];
+    int lungimeCamara, lungimeNecesare = 0, lungimeAvertizare, lungimeCumparaturi, lungimeRecuperare;
+
+    // CITIRE RETETE (TASTATURA/FISIER TEXT)
+    citesteReteteTastatura(listaNecesare, &lungimeNecesare);
+
+#if DEBUG == 1
+    puts("\nAFISARE INGREDIENTE NECESARE\n");
+    afiseazaLista(listaNecesare, lungimeNecesare);
+    puts("\nSFARSIT\n");
+#endif
 
     FILE *camara;
     if ((camara = fopen("camara.dat", "rb")) == NULL)
@@ -142,11 +254,45 @@ int main()
     {
         fseek(camara, 0L, SEEK_END);
         lungimeCamara = (int)(ftell(camara) / sizeof(Ingredient));
+        fseek(camara, 0L, SEEK_SET);
         fread(listaCamara, sizeof(Ingredient), lungimeCamara, camara);
+
+        //OPTIONAL (ignore it. e pt debug)
+        fseek(camara, 0L, SEEK_SET);
+        lungimeRecuperare = lungimeCamara;
+        fread(recuperare, sizeof(Ingredient), lungimeRecuperare, camara);
     }
+    fclose(camara);
 
     if (functie(listaCamara, lungimeCamara, listaNecesare, lungimeNecesare, listaAvertizare, &lungimeAvertizare, listaCumparaturi, &lungimeCumparaturi))
         printf("Bucatarul se poate apuca de meniu");
+    else
+        printf("Bucatarul nu are toate ingredientele");
     if (existaAvertizari(lungimeAvertizare))
         printf("Exista avertizari");
+
+#if DEBUG == 1
+
+    puts("\nAFISARE LISTA AVERTIZARE\n");
+    for (int i = 0; i < lungimeAvertizare; i++)
+        puts(listaAvertizare[i]);
+    puts("\nSFARSIT\n");
+
+    puts("\nAFISARE LISTA CUMPARATURI\n");
+    afiseazaLista(listaCumparaturi, lungimeCumparaturi);
+    puts("\nSFARSIT\n");
+
+    puts("\nAFISARE CAMARA PENTRU A VEDEA DIFERENTELE\n");
+    afiseazaCamara();
+#endif
+
+    // OPTIONAL (ignore this)
+    camara = fopen("camara.dat", "wb");
+    fwrite(recuperare, sizeof(Ingredient), lungimeRecuperare, camara);
+    fclose(camara);
+
+#if DEBUG == 1
+    puts("REINITIALIZARE CAMARA SI AFISARE");
+    afiseazaCamara();
+#endif
 }
