56f23a9779c474069261addbc4414da55f0c4d0b
MobR10 <100953132+MobR10@users.noreply.github.com>
2025-03-25 17:57:01 +0200

    PA-tema4-mergeSort-done-v1

--- PA/teme/tema4/tema4.c
+++ PA/teme/tema4/tema4.c
@@ -1,64 +1,86 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-int array[10];
-
-int* mergeArrays(int *leftArray,int *rightArray,int left, int middle,int right)
-{
-    //int *array=(int*)malloc(sizeof(int)*(right-left+1));
-    int i=left,j=middle+1,index=0;
-    while(i<=middle&&j<=right)
-    {
-        if(leftArray[i]>rightArray[j])
-            {
-                array[index]=rightArray[j];
-                index++;
-                j++;
-            }
-        else
-            {
-                array[index]=leftArray[i];
-                index++;
-                i++;
-            }
-    }
-    while(i<=middle)
-    {
-        array[index]=leftArray[i];
-        index++;
-        i++;
-    }
-    while(j<=right)
-    {
-        array[index]=rightArray[j];
-        index++;
-        j++;
-    }
-    return array;
-}
-
-int * mergeSort(int *v,int left,int right)
-{
-    if(left==right)
-        return v+left;
-
-    int middle=(left+right)/2;
-    int *leftArray=mergeSort(v,left,middle);
-    int *rightArray=mergeSort(v,middle+1,right);
-
-    return mergeArrays(leftArray,rightArray,left,middle,right);
-
-}
-
 void display(int *v,int n)
 {
     for(int i=0;i<n;i++)
         printf("%d ",v[i]);
 }
 
+void swap(int *a,int *b)
+{
+    int aux=*a;
+    *a=*b;
+    *b=aux;
+}
+void mergeArrays(int *v,int left,int middle,int right)
+{
+    int leftLength,rightLength;
+    leftLength=middle-left+1;
+    rightLength=right-middle;
+    int *leftArray=(int*)malloc(sizeof(int)*leftLength),*rightArray=(int*)malloc(sizeof(int)*rightLength);
+    for(int i=0;i<leftLength;i++)
+    {
+        leftArray[i]=v[left+i];
+    }
+    for(int i=0;i<rightLength;i++)
+    {
+        rightArray[i]=v[middle+1+i];
+    }
+
+    int i=0,j=0;
+
+    while(i<leftLength&&j<rightLength)
+    {
+        if(leftArray[i]<rightArray[j])
+            {
+                v[left]=leftArray[i];
+                i++;
+            }
+        else
+        {
+            v[left]=rightArray[j];
+            j++;
+        }
+        left++;
+    }
+    while(i<leftLength)
+    {
+        v[left]=leftArray[i];
+        i++;
+        left++;
+    }
+    while(j<rightLength)
+    {
+        v[left]=rightArray[j];
+        j++;
+        left++;
+    }
+    free(leftArray);
+    free(rightArray);
+}
+void mergeSort(int *v,int left,int right)
+{
+    if(left<right)
+    {
+        int middle=(left+right)/2;
+        mergeSort(v,left,middle);
+        mergeSort(v,middle+1,right);
+        mergeArrays(v,left,middle,right); 
+    }
+    
+
+}
+
+int comp(const void*a,const void *b)
+{
+    return *((int*)a)-*((int*)b);
+}
+
 int main()
 {
     int v[10]={-47,46,-91,-64,100,92,-20,83,-12,10};
-    int *w=mergeSort(v,0,9);
-    display(w,10);
+    mergeSort(v,0,9);
+    display(v,10);
+
 }
