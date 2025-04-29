1e885da6e4b54ced05392548f09751e2de57c52d
MobR10 <100953132+MobR10@users.noreply.github.com>
2025-04-29 22:27:44 +0300

    PA-tema7-done

--- PA/teme/tema7/main.c
+++ PA/teme/tema7/main.c
@@ -1,14 +1,15 @@
 #include <stdlib.h>
 #include <stdio.h>
 
-#define QUEUE_MAX 50
+#define QUEUE_MAX 50 // maximum number of elements in queue (default is defined in stacks_queues.h)
 #define STACK_MAX 50
 
-#include "stacks_queues.h"
+#include "stacks_queues.h" // my file containing queue and stack structures
 
 struct node
 {
-    int data;
+
+    int index;
     struct node *next;
 };
 typedef struct node Node;
@@ -16,52 +17,128 @@ typedef struct node Node;
 struct graph
 {
     int vertices;
+    int *values,*setValues; // values[i] is the value for node i and setValues[i]=0/1 whether a node's value has been set (for when the node is first created)
     int *visited;
     Node **adjacencyList;
+    Queue *front, *rear;
 };
 typedef struct graph Graph;
 
 Node *createNode(int data)
 {
-    Node *new = (Node *)malloc(sizeof(Node));
-    checkAllocation(new);
-    new->data = data;
-    new->next = NULL;
-    return new;
+    Node *node = (Node *)malloc(sizeof(Node));
+    checkAllocation(node);
+    node->index = data;
+    node->next = NULL;
+    return node;
 }
 
-Graph *createGraph(int vertices)
+Graph *createGraph()
 {
     Graph *graph = (Graph *)malloc(sizeof(Graph));
-    graph->vertices = vertices;
-    graph->adjacencyList = (Node **)malloc((unsigned long long int)(vertices + 1) * sizeof(Node *));
-
-    graph->visited = (int *)malloc(sizeof(int) * (unsigned long long int)(vertices + 1));
-    for (int i = 1; i <= vertices; i++)
-    {
-        graph->adjacencyList[i] = NULL;
-        graph->visited[i] = 0;
-    }
+    graph->vertices = 0;
+    graph->adjacencyList = NULL;
+    graph->visited = NULL;
+    graph->values = NULL;
+    graph->setValues=NULL;
+    graph->front = NULL;
+    graph->rear = NULL;
     return graph;
 }
 
-// function that adds an edge between 2 nodes in a not oriented graph
-void addEdgeNotOriented(Graph *graph, int source, int destination)
+void allocateNode(Graph *graph)
 {
-
-    Node *node = createNode(source);
-    node->next = graph->adjacencyList[destination];
-    graph->adjacencyList[destination] = node;
-    node = createNode(destination);
-    node->next = graph->adjacencyList[source];
-    graph->adjacencyList[source] = node;
+    if (isEmpty(graph))
+    {
+        puts("The graph is NULL. Can't add nodes! You must first create the graph, using createGraph().\n");
+        return;
+    }
+    if (graph->vertices == 0)
+    {
+        graph->adjacencyList = (Node **)malloc(sizeof(Node *));
+        graph->visited = (int *)malloc(sizeof(int));
+        graph->values = (int *)malloc(sizeof(int));
+        graph->setValues = (int *)malloc(sizeof(int));
+    }
+    else
+    {
+        graph->adjacencyList = (Node **)realloc(graph->adjacencyList, (size_t)(graph->vertices + 1) * sizeof(Node *));
+        graph->visited = (int *)realloc(graph->visited, (size_t)(graph->vertices + 1) * sizeof(int));
+        graph->values = (int *)realloc(graph->values, (size_t)(graph->vertices + 1) * sizeof(int));
+        graph->setValues = (int *)realloc(graph->setValues, (size_t)(graph->vertices + 1) * sizeof(int));
+    }
+    checkAllocation(graph->adjacencyList);
+    checkAllocation(graph->visited);
+    graph->adjacencyList[graph->vertices] = NULL;
+    graph->visited[graph->vertices] = 0;
+    graph->values[graph->vertices] = 0;
+    graph->setValues[graph->vertices] = 0;
+    graph->vertices++;
 }
 
-void addEdgeOriented(Graph *graph, int source, int destination)
+void setValue(Graph *graph,int index)
 {
-    Node *node = createNode(destination);
-    node->next = graph->adjacencyList[source];
-    graph->adjacencyList[source] = node;
+    printf("Introdu valoarea pentru nodul %d: ",index);
+    scanf("%d",&graph->values[index]);
+    graph->setValues[index]=1;
+}
+// function that adds one-way edge between 2 nodes (directed graph)
+int addOneEdge(Graph *graph, int sourceIndex, int destinationIndex)
+{
+    if (isEmpty(graph))
+    {
+        puts("The graph is NULL. Cant add edge! You must first create the graph. Use createGraph().\n");
+        return 0;
+    }
+    if (sourceIndex == destinationIndex)
+    {
+        puts("You cant add an edge between one and the same node.\n");
+        return 0;
+    }
+    if(sourceIndex < 0 || destinationIndex < 0)
+    {
+        puts("Source index or destination Index is negative. If you want to isolate a node, just don't call this function mentioning it and set its value manually!");
+        return 0;
+    }
+    // Dinamically allocate memory for as many nodes as necessary if the specified indexes are bigger than the current number of vertices
+    if (sourceIndex > graph->vertices - 1 || destinationIndex > graph->vertices - 1)
+        for (int i = graph->vertices; i < (sourceIndex > destinationIndex ? sourceIndex + 1 : destinationIndex + 1); i++)
+            allocateNode(graph);
+
+    Node *neighbours = graph->adjacencyList[sourceIndex];
+    while (neighbours)
+    {
+        if (neighbours->index == destinationIndex)
+        {
+            printf("Tried adding edge between nodes %d and %d, but there is already an edge!\n",sourceIndex,destinationIndex);
+            return 0;
+        }
+        neighbours = neighbours->next;
+    }
+
+    // Create the node to be added in the adjacency list of the source node
+    Node *node = createNode(destinationIndex);
+    checkAllocation(node);
+    node->next = graph->adjacencyList[sourceIndex];
+    graph->adjacencyList[sourceIndex] = node;
+
+    if(!graph->setValues[sourceIndex])
+        setValue(graph,sourceIndex);
+    if(!graph->setValues[destinationIndex])
+        setValue(graph,destinationIndex);
+    return 1;
+}
+
+// function that adds two-way edge between 2 nodes (undirected graph)
+void addTwoEdges(Graph *graph, int sourceIndex, int destinationIndex)
+{
+    if (isEmpty(graph))
+    {
+        puts("The graph is NULL. Cant add edge! You must first create the graph. Use createGraph().\n");
+        return;
+    }
+    if (addOneEdge(graph, sourceIndex, destinationIndex))
+        addOneEdge(graph, destinationIndex, sourceIndex);
 }
 
 void insertEdges(Graph *graph, int edges, int isOriented)
@@ -72,23 +149,23 @@ void insertEdges(Graph *graph, int edges, int isOriented)
     {
         scanf("%d%d", &src, &dest);
         if (!isOriented)
-            addEdgeNotOriented(graph, src, dest);
+            addTwoEdges(graph, src, dest);
         else
-            addEdgeOriented(graph, src, dest);
+            addOneEdge(graph, src, dest);
     }
 }
 
 void printGraph(Graph *graph)
 {
     int i;
-    for (i = 1; i <= graph->vertices; i++)
+    for (i = 1; i < graph->vertices; i++)
     {
-        Node *adjacencyList = graph->adjacencyList[i];
+        Node *neighbour = graph->adjacencyList[i];
         printf("%d: ", i);
-        while (adjacencyList)
+        while (neighbour)
         {
-            printf("%d ", adjacencyList->data);
-            adjacencyList = adjacencyList->next;
+            printf("%d ", neighbour->index);
+            neighbour = neighbour->next;
         }
         printf("\n");
     }
@@ -103,136 +180,227 @@ void wipeVisitedList(Graph *graph)
 }
 
 // parcurgeri
-void DFS(Graph *graph, int currentVertex)
+void dfsAlgorithm(Graph *graph, int currentVertex)
 {
-    Node *adjacencyList = graph->adjacencyList[currentVertex];
+    Node *neighbour = graph->adjacencyList[currentVertex];
 
     graph->visited[currentVertex] = 1;
     printf("%d->", currentVertex);
 
-    while (adjacencyList != NULL)
+    while (neighbour != NULL)
     {
-        int neighbour = adjacencyList->data;
-
-        if (graph->visited[neighbour] == 0)
+        if (graph->visited[neighbour->index] == 0)
         {
-            DFS(graph, neighbour);
+            dfsAlgorithm(graph, neighbour->index);
         }
-        adjacencyList = adjacencyList->next;
+        neighbour = neighbour->next;
     }
 }
-
-void BFS(Graph *graph, int start, Queue **front, Queue **rear)
+void DFS(Graph *graph,int currentVertex)
 {
-    Node *queue = NULL;
+    dfsAlgorithm(graph,currentVertex);
+    wipeVisitedList(graph);
+}
 
+void BFS(Graph *graph, int start)
+{
     graph->visited[start] = 1;
-    enqueue(front, rear, start);
+    enqueue(&graph->front, &graph->rear, start);
 
-    while (!isEmpty(queue))
+    while (!isEmpty(graph->front))
     {
-        int current = (*front)->data;
-        dequeue(front, rear);
-        printf("%d ", current);
+        int current = graph->front->data;
+        dequeue(&graph->front, &graph->rear);
+        printf("%d->", current);
 
-        Node *adjacencyList = graph->adjacencyList[current];
+        Node *neighbour = graph->adjacencyList[current];
 
-        while (adjacencyList)
+        while (neighbour)
         {
-            int neighbour = adjacencyList->data;
-
-            if (graph->visited[neighbour] == 0)
+            if (graph->visited[neighbour->index] == 0)
             {
-                graph->visited[neighbour] = 1;
-                enqueue(front, rear, neighbour);
+                graph->visited[neighbour->index] = 1;
+                enqueue(&graph->front, &graph->rear, neighbour->index);
             }
-            adjacencyList = adjacencyList->next;
+            neighbour = neighbour->next;
         }
     }
+    wipeVisitedList(graph);
 }
 
 // EX2
-int isPath(Graph *graph, int source, int destination)
+int isPath(Graph *graph, int source, int destination) 
 {
     if (source == destination)
         return 1;
 
     graph->visited[source] = 1;
 
-    Node *adjacencyList = graph->adjacencyList[source];
+    Node *neighbour = graph->adjacencyList[source];
 
-    while (adjacencyList != NULL)
+    while (neighbour != NULL)
     {
-        int neighbour = adjacencyList->data;
-        if (!graph->visited[neighbour])
+        if (!graph->visited[neighbour->index])
         {
-            if (isPath(graph, neighbour, destination))
+            if (isPath(graph, neighbour->index, destination))
                 return 1; // Path found through neighbor
         }
-        adjacencyList = adjacencyList->next;
+        neighbour = neighbour->next;
     }
 
     return 0; // No path found through any neighbors
 }
 
+// EX3
+// Function is called inside hasLoop() to count for the situation when the specified node is isolated, therefore it won't progress through the rest of graph
+int hasLoopAlgorithm(Graph *graph, int parent, int current)
+{
+    graph->visited[current] = 1;
+    Node *neighbour = graph->adjacencyList[current];
+
+    while (neighbour)
+    {
+        if (!graph->visited[neighbour->index])
+        {
+            if (hasLoopAlgorithm(graph, current, neighbour->index))
+            {
+                return 1;
+            }
+        }
+        else if (neighbour->index != parent)
+        {
+            return 1;
+        }
+        neighbour = neighbour->next;
+    }
+
+    return 0;
+}
+
+int hasLoop(Graph *graph, int parent, int current)
+{
+    // Check all nodes until one that is not isolated is reached
+    for(int i=0;i<graph->vertices;i++)
+        if(hasLoopAlgorithm(graph,i,i))
+            return 1;
+    return 0;
+}
+
+// EX4 cu chat gpt, efectiv nu pot. am citit si bineinteles ca am inteles, dar pur si simplu nu am reusit sa o fac eu :)
+int max(int a, int b) {
+    return a > b ? a : b;
+}
+
+// Helper function to find longest simple path between two nodes
+int dfsLongestPath(Graph *graph, int current, int destination, int *visited) {
+    if (current == destination) return 0; // reached destination
+
+    visited[current] = 1;  
+    int longest = -1;
+
+    Node *neighbour = graph->adjacencyList[current];
+    while (neighbour) {
+        if (!visited[neighbour->index]) {
+            int subPath = dfsLongestPath(graph, neighbour->index, destination, visited);
+            if (subPath != -1) {
+                longest = max(longest, 1 + subPath);
+            }
+        }
+        neighbour = neighbour->next;
+    }
+
+    visited[current] = 0;
+    return longest;
+}
+
+int findLongestValuePath(Graph *graph) {
+    if (isEmpty(graph)) return -1;
+
+    int minVal = 99;
+    int maxVal = -10;
+
+    // Find min and max values among nodes that have set values
+    for (int i = 0; i < graph->vertices; i++) {
+        if (graph->setValues[i]) {
+            if (graph->values[i] < minVal) minVal = graph->values[i];
+            if (graph->values[i] > maxVal) maxVal = graph->values[i];
+        }
+    }
+
+    // Get lists of all min and max nodes
+    int *minNodes = malloc((size_t)(graph->vertices) * sizeof(int));
+    int *maxNodes = malloc((size_t)(graph->vertices) * sizeof(int));
+    int minCount = 0, maxCount = 0;
+
+    for (int i = 0; i < graph->vertices; i++) {
+        if (!graph->setValues[i]) continue;
+        if (graph->values[i] == minVal) minNodes[minCount++] = i;
+        if (graph->values[i] == maxVal) maxNodes[maxCount++] = i;
+    }
+
+    // If no valid min or max nodes were found, exit early
+    if (minCount == 0 || maxCount == 0) {
+        free(minNodes);
+        free(maxNodes);
+        return -1;
+    }
+
+    int *visited = calloc((size_t)(graph->vertices), sizeof(int));
+    int longestPath = -1;
+
+    for (int i = 0; i < minCount; i++) {
+        for (int j = 0; j < maxCount; j++) {
+            int len = dfsLongestPath(graph, minNodes[i], maxNodes[j], visited);
+            longestPath = max(longestPath, len);
+        }
+    }
+
+    free(minNodes);
+    free(maxNodes);
+    free(visited);
+
+    return longestPath;
+}
+
+
 int main()
 {
-    // variables to be read and used to create both oriented and not oriented graph
-    int vertices;
-    int edges;
-    int startingVertex;
+    puts("EXERCITIUL 1:\n");
+    //  UNDIRECTED GRAPH
+    Graph *graph = createGraph();
 
-    Queue *front = NULL, *rear = NULL; // initialize queue for BFS
+    addTwoEdges(graph, 1, 4);
+    addTwoEdges(graph, 1, 5);
+    addTwoEdges(graph, 4, 6);
+    addTwoEdges(graph, 4, 3);
+    addTwoEdges(graph, 2, 3);
+    addTwoEdges(graph, 2, 6);
+    addTwoEdges(graph, 3, 5);
 
-    // NOT ORIENTED GRAPH
+    printf("\nDFS: "); DFS(graph,1);
+    puts("");
 
-    // printf("cate noduri are graful?");
-    // scanf("%d", &vertices);
+    printf("BFS: "); BFS(graph,1);
+    puts("");
 
-    // printf("cate muchii are graful?");
-    // scanf("%d", &edges);
+    puts("\nEXERCITIUL 2:\n");
+    // DIRECTED GRAPH
+    Graph *ograph = createGraph();
+    addOneEdge(ograph, 0, 1);
+    addOneEdge(ograph, 0, 2);
+    addOneEdge(ograph, 1, 3);
+    addOneEdge(ograph, 1, 2);
+    addOneEdge(ograph, 2, 3);
+    addOneEdge(ograph, 3, 4);
+    addOneEdge(ograph, 2, 4);
 
-    // Graph *graph = createGraph(vertices);
+    printf("\nExista drum intre nodurile %d si %d? Raspuns: %d\n",2,4,isPath(graph,2,4));
 
-    // insertEdges(graph, edges, 0);
-
-    // printf("de unde plecam in DFS?");
-    // scanf("%d", &startingVertex);
-
-    // printf("parcurgere cu DFS:");
-    // DFS(graph, startingVertex);
-
-    // wipeVisitedList(graph);
-    // printf("\n");
-
-    // printf("de unde plecam in BFS?");
-    // scanf("%d", &startingVertex);
-
-    // printf("parcurgere cu BFS:");
-    // BFS(graph, startingVertex, &front, &rear);
-    // DFS(graph, 0);
-
-    // ORIENTED GRAPH
-
-    // printf("Cate noduri sa aiba graful orientat? ");
-    // scanf("%d", &vertices);
-    // Graph *ograph = createGraph(vertices);
-
-    // printf("Cate muchii? ");
-    // scanf("%d", &edges);
-
-    // insertEdges(ograph, edges, 1);
-
-    Graph *ograph=createGraph(5);
-    addEdgeOriented(ograph,0,1);
-    addEdgeOriented(ograph,0,2);
-    addEdgeOriented(ograph,1,3);
-    addEdgeOriented(ograph,1,2);
-    addEdgeOriented(ograph,2,3);
-    addEdgeOriented(ograph,3,4);
-    addEdgeOriented(ograph,2,4);
-
-    printf("%d",isPath(ograph,2,4));
+    puts("\nEXERCITIUL 3:\n");
+    wipeVisitedList(graph);
+    printf("Exista cicluri in primul graf (da sau nu / 1 sau 0): %d.\n",hasLoop(graph,0,0));
+    puts("\nEXERCITIUL 4:\n");
+    printf("Cel mai lung drum de la cel mai mic nod la cel mai mare in primul graf este: %d.",findLongestValuePath(graph));
 
     return 0;
 }
